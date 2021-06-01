75ac7b2e832fe8b2464db5147dad240184482a2d
Marcos Paulo <mr.soulfox@gmail.com>
2021-06-01 12:18:36 -0300

    add link modal and update close modal

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,20 @@
-document.getElementById('close').addEventListener('click', close)
+function close(id) {
+    if (id == "qrcode") {        
+        document.querySelector('#qrcodeModal').style.animation = "close 0.45s"
+        document.querySelector('#qrcode').innerHTML = ''
+        document.querySelector('#modalContainer').style.display = 'none'
 
-function close() {
-    document.querySelector('#qrcodeModal').style.animation = "close 0.45s"
-    document.querySelector('#qrcode').innerHTML = ''
-    document.querySelector('#modalContainer').style.display = 'none'
+    } else if (id == "link") {
+        document.querySelector('#linkModal').style.animation = 'close 0.45s'
+        document.querySelector('#modalContainer').style.display = 'none'
+    }
+}
+
+function open() {
+    let modalContainer = document.querySelector('#modalContainer')
+    modalContainer.style.display = 'flex'
+    modalContainer.style.animation = 'asideModal 0.50s ease-in-out both'
+
+    document.querySelector('#linkModal').style.display = 'block'
+    document.querySelector('#linkModal').style.animation = "modal 0.45s ease-in-out both"
 }
