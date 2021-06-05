8e2f275be559d99dff88d84cde41113ad6f75a02
Marcos Paulo <mr.soulfox@gmail.com>
2021-06-05 15:42:25 -0300

    update redirects

--- js/redirect/redirect.js
+++ js/redirect/redirect.js
@@ -1,4 +1,33 @@
-function redirect() {
+async function redirect() {
     alert('Em construção')
-    document.location.replace('/')
+
+    let id = document.location.search
+    console.log(id)
+
+    if (id.length == 5) {
+        let data = await getOriginalLink(id)
+    
+        document.location.replace(data.link)
+
+    } else {
+        //document.location.replace('/')
+        alert('id incorreto')
+
+    }
+
+}
+
+async function getOriginalLink(id) {
+    const newId = encodeURIComponent(id)
+
+    try {
+        const response = await fetch(`https://bitil.herokuapp.com/visit/${newId}`)
+        const data = await response.json()
+
+        return data
+
+    } catch (error) {
+        console.log('Deu erro')
+
+    }
 }
